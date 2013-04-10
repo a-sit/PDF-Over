78fb5e06db37127898c1446102354b097a789f79
tkellner <tkellner@174cde9d-5d70-4d2a-aa98-46368bc2aaf7>
2013-04-10 19:14:16 +0000

    + fixed auto positioning
    git-svn-id: https://joinup.ec.europa.eu/svn/pdf-over/trunk@214 174cde9d-5d70-4d2a-aa98-46368bc2aaf7

--- pdf-over-gui/src/main/java/at/asit/pdfover/gui/workflow/states/PositioningState.java
+++ pdf-over-gui/src/main/java/at/asit/pdfover/gui/workflow/states/PositioningState.java
@@ -108,6 +108,8 @@ public class PositioningState extends State {
 			}
 			
 			this.positionComposite.requestFocus();
+		} else {
+			this.setNextState(new BKUSelectionState(this.stateMachine));
 		}
 	}
 
