76515f10a45a5c4a22694589ce9d733bad2393a2
tkellner <tkellner@174cde9d-5d70-4d2a-aa98-46368bc2aaf7>
2013-04-10 19:30:38 +0000

    Add app on MacOS X
    git-svn-id: https://joinup.ec.europa.eu/svn/pdf-over/trunk@356 174cde9d-5d70-4d2a-aa98-46368bc2aaf7

---
+++ pdf-over-gui/src/main/resources/scripts/postinstall_mac.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+BASEDIR=$(cd "$(dirname "$0")"; pwd)
+export LC_CTYPE="UTF-8"
+cd "$BASEDIR"
+chmod a+x pdf-over_mac.sh
+cd ..
+osacompile -e "do shell script \"$BASEDIR/pdf-over_mac.sh\"" -x -o PDF-Over.app
