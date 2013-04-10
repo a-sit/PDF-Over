3f69a54ce7268c1bdf3e5a8ad9fa45880f003d8f
tkellner <tkellner@174cde9d-5d70-4d2a-aa98-46368bc2aaf7>
2013-04-10 19:32:40 +0000

    Extract basic mobile BKU status stuff
    git-svn-id: https://joinup.ec.europa.eu/svn/pdf-over/trunk@375 174cde9d-5d70-4d2a-aa98-46368bc2aaf7

--- pdf-over-gui/src/main/java/at/asit/pdfover/gui/workflow/states/mobilebku/ATrustStatus.java
+++ pdf-over-gui/src/main/java/at/asit/pdfover/gui/workflow/states/mobilebku/ATrustStatus.java
@@ -22,9 +22,9 @@ import org.slf4j.LoggerFactory;
 import at.asit.pdfover.gui.workflow.ConfigProvider;
 
 /**
- * 
+ * A-Trust MobileBKUStatus implementation
  */
-public class ATrustStatus implements MobileBKUStatus {
+public class ATrustStatus extends AbstractMobileBKUStatusImpl {
 	/**
 	 * SLF4J Logger instance
 	 **/
@@ -39,16 +39,6 @@ public class ATrustStatus implements MobileBKUStatus {
 
 	private String viewstate;
 	private String eventvalidation;
-	private String sessionID;
-	private String phoneNumber;
-	private String mobilePassword;
-	private String baseURL;
-	private String refVal;
-	private String errorMessage;
-	private String tan;
-	private String server;
-	private int tanTries = MOBILE_MAX_TAN_TRIES;
-
 	/**
 	 * Constructor
 	 * @param provider 
@@ -64,86 +54,6 @@ public class ATrustStatus implements MobileBKUStatus {
 	@Override
 	public int getMaxTanTries() {
 		return MOBILE_MAX_TAN_TRIES;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#getTanTries()
-	 */
-	@Override
-	public int getTanTries() {
-		return this.tanTries;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#setTanTries(int)
-	 */
-	@Override
-	public void setTanTries(int tries) {
-		this.tanTries = tries;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#getTan()
-	 */
-	@Override
-	public String getTan() {
-		return this.tan;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#setTan(java.lang.String)
-	 */
-	@Override
-	public void setTan(String tan) {
-		this.tan = tan;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#getErrorMessage()
-	 */
-	@Override
-	public String getErrorMessage() {
-		return this.errorMessage;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#setErrorMessage(java.lang.String)
-	 */
-	@Override
-	public void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#getRefVal()
-	 */
-	@Override
-	public String getRefVal() {
-		return this.refVal;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#setRefVal(java.lang.String)
-	 */
-	@Override
-	public void setRefVal(String refVal) {
-		this.refVal = refVal;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#getBaseURL()
-	 */
-	@Override
-	public String getBaseURL() {
-		return this.baseURL;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#setBaseURL(java.lang.String)
-	 */
-	@Override
-	public void setBaseURL(String baseURL) {
-		this.baseURL = baseURL;
 	}
 
 	/**
@@ -173,69 +83,5 @@ public class ATrustStatus implements MobileBKUStatus {
 	 */
 	public void setEventvalidation(String eventvalidation) {
 		this.eventvalidation = eventvalidation;
-	}
-	
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#getPhoneNumber()
-	 */
-	@Override
-	public String getPhoneNumber() {
-		return this.phoneNumber;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#setPhoneNumber(java.lang.String)
-	 */
-	@Override
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#getMobilePassword()
-	 */
-	@Override
-	public String getMobilePassword() {
-		return this.mobilePassword;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#setMobilePassword(java.lang.String)
-	 */
-	@Override
-	public void setMobilePassword(String mobilePassword) {
-		this.mobilePassword = mobilePassword;
-	}
-	
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#getSessionID()
-	 */
-	@Override
-	public String getSessionID() {
-		return this.sessionID;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#setSessionID(java.lang.String)
-	 */
-	@Override
-	public void setSessionID(String sessionID) {
-		this.sessionID = sessionID;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#getServer()
-	 */
-	@Override
-	public String getServer() {
-		return this.server;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.asit.pdfover.gui.workflow.states.mobilebku.MobileBKUStatus#setServer(java.lang.String)
-	 */
-	@Override
-	public void setServer(String server) {
-		this.server = server;
 	}
 }
