5793f0dc194ae3519de7a12808d99aa2a555cd73
tkellner <tkellner@174cde9d-5d70-4d2a-aa98-46368bc2aaf7>
2013-04-10 18:50:35 +0000

    StateMachine Proposal
    git-svn-id: https://joinup.ec.europa.eu/svn/pdf-over/trunk@10 174cde9d-5d70-4d2a-aa98-46368bc2aaf7

---
+++ pdf-over/pdf-gui/src/main/java/at/asit/pdfover/gui/DeveloperMain.java
@@ -0,0 +1,18 @@
+package at.asit.pdfover.gui;
+
+import at.asit.pdfover.gui.workflow.Workflow;
+
+public class DeveloperMain {
+
+	/**
+	 * Developer Main Entry point...
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Workflow flow = new Workflow();
+		flow.SetCmdArgs(args);
+		
+		flow.Start();
+	}
+
+}
