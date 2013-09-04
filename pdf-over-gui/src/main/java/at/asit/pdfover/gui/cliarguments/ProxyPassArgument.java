45ecf17a1526aad782e1ee90520297173db4db29
tkellner <tkellner@174cde9d-5d70-4d2a-aa98-46368bc2aaf7>
2013-09-04 13:36:01 +0000

    Proxy arguments classes
    git-svn-id: https://joinup.ec.europa.eu/svn/pdf-over/trunk@475 174cde9d-5d70-4d2a-aa98-46368bc2aaf7

---
+++ pdf-over-gui/src/main/java/at/asit/pdfover/gui/cliarguments/ProxyPassArgument.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2012 by A-SIT, Secure Information Technology Center Austria
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * http://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+package at.asit.pdfover.gui.cliarguments;
+
+// Imports
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import at.asit.pdfover.gui.exceptions.InitializationException;
+import at.asit.pdfover.gui.utils.Messages;
+
+/**
+ * CLI Argument to provide the proxy password
+ */
+public class ProxyPassArgument extends Argument {
+	/**
+	 * Constructor
+	 */
+	public ProxyPassArgument() {
+		super(new String[] {"-proxypass"}, "argument.help.proxypass"); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+	/**
+	 * SLF4J Logger instance
+	 **/
+	private static final Logger log = LoggerFactory
+			.getLogger(ProxyPassArgument.class);
+
+	/* (non-Javadoc)
+	 * @see at.asit.pdfover.gui.cliarguments.CLIArgument#handleArgument(java.lang.String[], int, at.asit.pdfover.gui.workflow.StateMachine, at.asit.pdfover.gui.cliarguments.ArgumentHandler)
+	 */
+	@Override
+	public int handleArgument(String[] args, int argOffset,
+			ArgumentHandler handler)
+			throws InitializationException {
+		try {
+			if (args.length > argOffset + 1) {
+
+				String proxyPass = args[argOffset + 1];
+				
+				getConfiguration().setProxyPassOverlay(proxyPass);
+				
+				return argOffset + 1;
+			}
+		} catch (Exception ex) {
+			log.error("Proxy password argument invalid!", ex); //$NON-NLS-1$
+			throw new InitializationException(
+					Messages.getString("argument.invalid.proxypass") + this.getHelpText(), ex); //$NON-NLS-1$
+		}
+
+		throw new InitializationException(
+				Messages.getString("argument.invalid.proxypass") + this.getHelpText(), null); //$NON-NLS-1$
+	}
+
+}
---
+++ pdf-over-gui/src/main/java/at/asit/pdfover/gui/cliarguments/ProxyUserArgument.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2012 by A-SIT, Secure Information Technology Center Austria
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * http://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+package at.asit.pdfover.gui.cliarguments;
+
+// Imports
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import at.asit.pdfover.gui.exceptions.InitializationException;
+import at.asit.pdfover.gui.utils.Messages;
+
+/**
+ * CLI Argument to provide the proxy username
+ */
+public class ProxyUserArgument extends Argument {
+	/**
+	 * Constructor
+	 */
+	public ProxyUserArgument() {
+		super(new String[] {"-proxyuser"}, "argument.help.proxyuser"); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+	/**
+	 * SLF4J Logger instance
+	 **/
+	private static final Logger log = LoggerFactory
+			.getLogger(ProxyUserArgument.class);
+
+	/* (non-Javadoc)
+	 * @see at.asit.pdfover.gui.cliarguments.CLIArgument#handleArgument(java.lang.String[], int, at.asit.pdfover.gui.workflow.StateMachine, at.asit.pdfover.gui.cliarguments.ArgumentHandler)
+	 */
+	@Override
+	public int handleArgument(String[] args, int argOffset,
+			ArgumentHandler handler)
+			throws InitializationException {
+		try {
+			if (args.length > argOffset + 1) {
+
+				String proxyUser = args[argOffset + 1];
+				
+				getConfiguration().setProxyUserOverlay(proxyUser);
+				
+				return argOffset + 1;
+			}
+		} catch (Exception ex) {
+			log.error("Proxy username argument invalid!", ex); //$NON-NLS-1$
+			throw new InitializationException(
+					Messages.getString("argument.invalid.proxyuser") + this.getHelpText(), ex); //$NON-NLS-1$
+		}
+
+		throw new InitializationException(
+				Messages.getString("argument.invalid.proxyuser") + this.getHelpText(), null); //$NON-NLS-1$
+	}
+
+}
