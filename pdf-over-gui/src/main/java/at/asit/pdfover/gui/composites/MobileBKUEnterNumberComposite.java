1902a618addd64eea104f8d96a113c9a266f800b
Andreas Abraham <andreas.abraham@egiz.gv.at>
2021-08-13 09:54:42 +0200

    Code cleanup

--- pdf-over-gui/src/main/java/at/asit/pdfover/gui/composites/MobileBKUEnterNumberComposite.java
+++ pdf-over-gui/src/main/java/at/asit/pdfover/gui/composites/MobileBKUEnterNumberComposite.java
@@ -95,8 +95,6 @@ public class MobileBKUEnterNumberComposite extends StateComposite {
 				MobileBKUEnterNumberComposite.this.txt_number.setFocus();
 				return;
 			}
-
-			//MobileBKUEnterNumberComposite.this.state.updateStateMachine();
 		}
 	};
 
@@ -218,7 +216,6 @@ public class MobileBKUEnterNumberComposite extends StateComposite {
 		
 		this.txt_number = new Text(containerComposite, SWT.SINGLE | SWT.NATIVE | SWT.BORDER);
 		FormData fd_number = new FormData();
-		//fd_number.top = new FormAttachment(0, 20);
 		fd_number.bottom = new FormAttachment(50, -10);
 		fd_number.left = new FormAttachment(50, 10);
 		fd_number.right = new FormAttachment(100, -20);
@@ -230,9 +227,7 @@ public class MobileBKUEnterNumberComposite extends StateComposite {
 		this.lbl_number.setAlignment(SWT.RIGHT);
 		this.lbl_number.setText(Messages.getString("mobileBKU.number")); //$NON-NLS-1$
 		FormData fd_lbl_number = new FormData();
-		//fd_lbl_number.top = new FormAttachment(30, -15);
 		fd_lbl_number.bottom = new FormAttachment(50, -10);
-		//fd_lbl_number.left = new FormAttachment(0, 20);
 		fd_lbl_number.right = new FormAttachment(50, -10);
 		this.lbl_number.setLayoutData(fd_lbl_number);
 
@@ -254,7 +249,6 @@ public class MobileBKUEnterNumberComposite extends StateComposite {
 				| SWT.BORDER | SWT.NATIVE);
 		FormData fd_password = new FormData();
 		fd_password.top = new FormAttachment(50, 10);
-		//fd_password.bottom = new FormAttachment(50, 15);
 		fd_password.left = new FormAttachment(50, 10);
 		fd_password.right = new FormAttachment(100, -20);
 		this.txt_password.setLayoutData(fd_password);
@@ -265,28 +259,22 @@ public class MobileBKUEnterNumberComposite extends StateComposite {
 		this.lbl_password.setText(Messages.getString("mobileBKU.password")); //$NON-NLS-1$
 		FormData fd_lbl_password = new FormData();
 		fd_lbl_password.top = new FormAttachment(50, 10);
-		//fd_lbl_password.bottom = new FormAttachment(50, 15);
-		//fd_lbl_password.left = new FormAttachment(0, 20);
 		fd_lbl_password.right = new FormAttachment(50, -10);
 		this.lbl_password.setLayoutData(fd_lbl_password);
 
 		this.btn_ok = new Button(containerComposite, SWT.NATIVE);
 		this.btn_ok.setText(Messages.getString("common.Ok")); //$NON-NLS-1$
 		FormData fd_btn_ok = new FormData();
-		//fd_btn_ok.top = new FormAttachment(87, 0);
 		fd_btn_ok.bottom = new FormAttachment(100, -20);
 		fd_btn_ok.right = new FormAttachment(100, -20);
-		//fd_btn_ok.left = new FormAttachment(100, -70);
 		this.btn_ok.setLayoutData(fd_btn_ok);
 		this.btn_ok.addSelectionListener(this.okListener);
 
 		this.btn_cancel = new Button(containerComposite, SWT.NATIVE);
 		this.btn_cancel.setText(Messages.getString("common.Cancel")); //$NON-NLS-1$
 		FormData fd_btn_cancel = new FormData();
-		//fd_btn_cancel.top = new FormAttachment(87, 0);
 		fd_btn_cancel.bottom = new FormAttachment(100, -20);
 		fd_btn_cancel.right = new FormAttachment(this.btn_ok, -10);
-		//fd_btn_cancel.left = new FormAttachment(100, -70);
 		this.btn_cancel.setLayoutData(fd_btn_cancel);
 		this.btn_cancel.addSelectionListener(this.cancelListener);
 
@@ -312,7 +300,7 @@ public class MobileBKUEnterNumberComposite extends StateComposite {
 		this.btn_ok.setEnabled(true);
 	}
 	
-	/*
+	/**
 	 * (non-Javadoc)
 	 * 
 	 * @see at.asit.pdfover.gui.composites.StateComposite#doLayout()
@@ -367,7 +355,7 @@ public class MobileBKUEnterNumberComposite extends StateComposite {
 		}
 	}
 
-	/* (non-Javadoc)
+	/** (non-Javadoc)
 	 * @see at.asit.pdfover.gui.composites.StateComposite#reloadResources()
 	 */
 	@Override
