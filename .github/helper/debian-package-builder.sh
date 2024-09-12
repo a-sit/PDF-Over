a872a815c832708d117d6c756602364c30f39297
Stefan <stefan.more@a-sit.at>
2024-09-12 19:23:02 +0200

    Upload deb package; fetch full repo for changelog

--- .github/helper/debian-package-builder.sh
+++ .github/helper/debian-package-builder.sh
@@ -70,7 +70,7 @@ fpm \
   icons/icon144x144.png=/usr/share/pixmaps/$NAME.png
 
 # next: upload/publish $NAME-$VERSION-$ARCH.deb
-
+cp $NAME-$VERSION-$ARCH.deb $NAME.deb
 
 #mkdir experiments
 #mv $NAME-$VERSION-$ARCH.deb experiments
