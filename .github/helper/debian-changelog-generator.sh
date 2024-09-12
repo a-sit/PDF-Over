1b5c8f8308fc4f2288d02071c63616bacc31d6e0
Stefan <stefan.more@a-sit.at>
2024-09-12 19:07:23 +0200

    Added CI Debian Package Builder

---
+++ .github/helper/debian-changelog-generator.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
+
+pwd
+cd ./pdf-over-build
+ls
+
+
+mkdir debian
+touch debian/changelog
+
+prevtag="pdf-over-4.0.0"
+pkgname="pdf-over-nightly"
+git tag -l  | sort -V | while read tag; do
+    (echo -e "$pkgname (${tag#pdf-over-}); urgency=medium \n"; git log --pretty=format:'  * %s' $prevtag..$tag; git log --pretty='format:%n%n -- %aN <%aE>  %aD%n%n' $tag^..$tag) | cat - debian/changelog | sponge debian/changelog
+        prevtag=$tag
+done
+
+tag=`git tag -l v* | sort -V | tail -1`
+[ `git log --exit-code $tag..HEAD | wc -l` -ne 0 ] && git-dch -s $tag -S --no-multimaint --nmu --ignore-branch --snapshot-number="'{:%Y%m%d%H%M%S}'.format(__import__('datetime').datetime.fromtimestamp(`git log -1 --pretty=format:%at`))"
+
+#sed -i 's/UNRELEASED/unstable/' debian/changelog
+
+echo "done"
--- .github/helper/debian-package-builder.sh
+++ .github/helper/debian-package-builder.sh
@@ -3,5 +3,75 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
-echo "Hi!"
 
+# config:
+NAME="pdf-over-nightly"
+FULLNAME="PDF-Over Nightly"
+VERSION="`date +"%Y%m%d.%H%M%S"`"
+ARCH="all"
+JAR_TARGET="/usr/share/java/$NAME"
+
+
+pwd
+cd ./pdf-over-build
+ls
+
+
+jar_files=""
+cd lib
+for file in *.jar; do
+    if [ -f "$file" ]; then
+        jar_files+="lib/$file=$JAR_TARGET/$file "
+    fi
+done
+cd ..
+
+
+cat > $NAME.sh << EOF
+#!/bin/sh 
+# PDF-Over ($NAME $VERSION) launcher, generated on `date`
+
+GDK_BACKEND=x11,wayland exec java -cp "$JAR_TARGET/*" at.asit.pdfover.gui.Main "\$@"
+EOF
+chmod +x $NAME.sh
+ 
+
+cat > $NAME.desktop << EOF
+[Desktop Entry]
+Version=$VERSION
+Type=Application
+Name=$FULLNAME
+Comment=Create PAdES conforming PDF signatures
+Exec=$NAME
+Icon=$NAME
+Terminal=false
+StartupNotify=false
+Categories=Office
+EOF
+
+
+fpm \
+  -s dir -t deb \
+  -p $NAME-$VERSION-$ARCH.deb \
+  --name $NAME \
+  --license EUPL-1.2 \
+  --version $VERSION \
+  --architecture $ARCH \
+  --deb-upstream-changelog changelog \
+  --depends bash --depends "default-jre | java-runtime (>= 17)" \
+  --description "PDF-Over is your tool for frequent & efficient PDF signing." \
+  --url "https://technology.a-sit.at/en/pdf-over/" \
+  --vendor "A-SIT <software@egiz.gv.at>" \
+  --maintainer "A-SIT <software@egiz.gv.at>" \
+  $jar_files \
+  $NAME.sh=/usr/bin/$NAME \
+  $NAME.desktop=/usr/share/applications/$NAME.desktop \
+  icons/icon144x144.png=/usr/share/pixmaps/$NAME.png
+
+# next: upload/publish $NAME-$VERSION-$ARCH.deb
+
+
+#mkdir experiments
+#mv $NAME-$VERSION-$ARCH.deb experiments
+#cd experiments
+#ar x $NAME-$VERSION-$ARCH.deb
