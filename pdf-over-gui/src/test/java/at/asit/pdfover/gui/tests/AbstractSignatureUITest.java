c1f940bf998a3997c30023b8c5fa3f8038c5c212
kathrin.resek <kathrin.resek@a-sit.at>
2025-03-03 10:36:03 +0100

    extend test - sign document

--- pdf-over-gui/src/test/java/at/asit/pdfover/gui/tests/AbstractSignatureUITest.java
+++ pdf-over-gui/src/test/java/at/asit/pdfover/gui/tests/AbstractSignatureUITest.java
@@ -7,6 +7,7 @@ import at.asit.pdfover.gui.Main;
 import at.asit.pdfover.gui.workflow.StateMachine;
 import at.asit.pdfover.gui.workflow.config.ConfigurationManager;
 import lombok.NonNull;
+import org.apache.commons.io.FilenameUtils;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -23,10 +24,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.*;
-        import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AbstractSignatureUITest {
 
@@ -38,6 +39,7 @@ public class AbstractSignatureUITest {
     private static final File inputFile = new File("src/test/resources/TestFile.pdf");
     private static final String outputDir = inputFile.getAbsoluteFile().getParent();
     private Profile currentProfile = Profile.SIGNATURBLOCK_SMALL;
+    private final String postFix = "_superSigned";
 
     private static final Logger logger = LoggerFactory
             .getLogger(AbstractSignatureUITest.class);
@@ -95,11 +97,22 @@ public class AbstractSignatureUITest {
             ICondition widgetExists = new WidgetExistsCondition(str("mobileBKU.number"));
             bot.waitUntil(widgetExists, 20000);
             bot.textWithLabel(str("mobileBKU.number")).setText("TestUser-1902503362");
+            bot.textWithLabel(str("mobileBKU.password")).setText("123456789");
+            bot.button(str("common.Ok")).click();
         }
         catch (WidgetNotFoundException wnf) {
             bot.button(str("common.Cancel")).click();
             fail(wnf.getMessage());
         }
+
+        File output = new File(getPathOutputFile());
+        ICondition outputExists = new FileExistsCondition(output);
+        bot.waitUntil(outputExists, 20000);
+
+        if(!output.exists()) {
+            bot.button(str("common.Cancel")).click();
+        }
+        assertTrue(output.exists(), "Received signed PDF");
     }
 
     private static void setProperty(@NonNull Properties props, @NonNull String key, @NonNull String value) { props.setProperty(key, value); }
@@ -108,7 +121,6 @@ public class AbstractSignatureUITest {
         ConfigurationManager cm = new ConfigurationManager();
         Point size = cm.getMainWindowSize();
 
-        String postFix = "_superSigned";
         Map<String, String> testParams = Map.ofEntries(
                 Map.entry(Constants.CFG_BKU, cm.getDefaultBKUPersistent().name()),
                 Map.entry(Constants.CFG_KEYSTORE_PASSSTORETYPE, "memory"),
@@ -133,6 +145,22 @@ public class AbstractSignatureUITest {
         } catch (IOException e) {
             logger.warn("Failed to create configuration file.");
         }
+    }
+
+    /**
+     * Returns path of the signed document.
+     */
+    private String getPathOutputFile() {
+        String fileNameSigned = inputFile
+                .getName()
+                .substring(0, inputFile.getName().lastIndexOf('.'))
+                .concat(postFix)
+                .concat(".pdf");
+        String pathOutputFile = FilenameUtils.separatorsToSystem(outputDir
+                .concat("/")
+                .concat(fileNameSigned));
+        assertNotNull(pathOutputFile);
+        return pathOutputFile;
     }
 
 }
